293fa3a27fada229b98c15686432665d951bca9e
Danish Mughal <mughal.danish@gmail.com>
2013-06-16 03:58:22 -0700

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WikiflowApi::Application.config.secret_token = '42959a6e04817dbe0f3e5f4799b7fb1bc0b2aa24fb59665e332f0e6c5166fbe41d6f504e6beee62969badf7f8976711afdae6de16f826ae6ddbe9acbd0694759'
